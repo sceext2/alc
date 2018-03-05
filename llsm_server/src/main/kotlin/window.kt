4d384cde9aec9523c66b22649893b71e2045a9ee
sceext2 <sceext@foxmail.com>
2018-03-05 18:13:13 +0800

    llsm_server: double-click to fullscreen, test OK

--- llsm_server/src/main/kotlin/window.kt
+++ llsm_server/src/main/kotlin/window.kt
@@ -12,6 +12,10 @@ import javafx.scene.image.PixelFormat
 import javafx.scene.image.ImageView
 import javafx.beans.value.ObservableValue
 import javafx.beans.value.ChangeListener
+import javafx.event.EventHandler
+import javafx.scene.input.KeyEvent
+import javafx.scene.input.KeyCode
+import javafx.scene.input.MouseEvent
 
 
 var main_window_size_x: Int = 1280
@@ -19,16 +23,19 @@ var main_window_size_y: Int = 720
 var main_window_title: String = ""
 var main_window: VideoWindow? = null
 
-
 class VideoWindow() : Application() {
     lateinit var image: WritableImage
     lateinit var writer: PixelWriter
     lateinit var iv: ImageView
+    lateinit var _stage: Stage
+    lateinit var root: Group
 
     var sx: Int = 0
     var sy: Int = 0
 
     override fun start(stage: Stage) {
+        _stage = stage
+
         main_window = this
         sx = main_window_size_x
         sy = main_window_size_y
@@ -45,7 +52,7 @@ class VideoWindow() : Application() {
         iv.setSmooth(true)
         iv.setCache(true)
 
-        val root = Group()
+        root = Group()
         root.getChildren().add(iv)
 
         val scene = Scene(root, sx.toDouble(), sy.toDouble())
@@ -59,9 +66,43 @@ class VideoWindow() : Application() {
         }
         scene.widthProperty().addListener(window_size_listener)
         scene.heightProperty().addListener(window_size_listener)
+        scene.setOnKeyTyped(object: EventHandler<KeyEvent> {
+            override fun handle(event: KeyEvent) {
+                when(event.code) {
+                    // F11 for fullscreen
+                    KeyCode.F11 -> {
+                        // TODO
+                        _change_fullscreen()
+                    }
+                    else -> {
+                        // nothing todo
+                    }
+                }
+            }
+        })
+        scene.setOnMouseClicked(object: EventHandler<MouseEvent> {
+            override fun handle(event: MouseEvent) {
+                //root.requestFocus()
+
+                // double click to fullscreen
+                if (event.clickCount > 1) {
+                    _change_fullscreen()
+                }
+            }
+        })
 
         stage.setScene(scene)
         stage.show()
+
+        root.requestFocus()
+    }
+
+    fun _change_fullscreen() {
+        if (_stage.isFullScreen()) {
+            _stage.setFullScreen(false)
+        } else {
+            _stage.setFullScreen(true)
+        }
     }
 
     fun _on_window_resize(size_x: Double, size_y: Double) {
@@ -72,11 +113,9 @@ class VideoWindow() : Application() {
     fun _do_update_frame(size_x: Int, size_y: Int, data: ByteArray) {
         // load pixel data in image
         writer.setPixels(0, 0, size_x, size_y, PixelFormat.getByteRgbInstance(), data, 0, size_x * 3)
-        // TODO
     }
 
     fun update_frame(size_x: Int, size_y: Int, data: ByteArray) {
-        // TODO
         Platform.runLater(object: Runnable {
             override fun run() {
                 _do_update_frame(size_x, size_y, data)
